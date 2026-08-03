07afc4c6e1d82934beb518932e13938c53404d21
Pranav <gaddampranavreddy@gmail.com>
2026-08-02 23:37:53 -0700

    core.c

---
+++ Day14/core.c
@@ -0,0 +1,39 @@
+//When you want to enable an interrupt (like UART RX), you don't just configure the UART. You must tell the ARM Core's NVIC (Nested Vectored Interrupt Controller) to allow that specific interrupt line to wake the CPU. The ISER (Interrupt Set-Enable Register) is an array of 32-bit registers.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct {
+    // Each bit in this array controls one of 256 possible interrupts.
+    // ISER[0] controls Interrupts 0-31. ISER[1] controls 32-63, etc.
+    uint32_t volatile ISER[8]; 
+} NVIC_TypeDef;
+
+void enable_nvic_irq(NVIC_TypeDef *nvic, uint8_t irq_number);
+void enable_nvic_irq(NVIC_TypeDef *nvic, uint8_t irq_number) {
+
+    uint32_t index = irq_number/32;
+
+    uint32_t position = irq_number % 32;
+
+    uint32_t *bit = nvic->ISER + index;
+
+    *bit |= 1;
+
+}
+
+int main(void){
+    NVIC_TypeDef sim_nvic = {0};
+
+    enable_nvic_irq(&sim_nvic, 37);
+
+    uint32_t *ptr = sim_nvic.ISER;
+
+    for (int x = 0; x < 8; x++){
+        printf("ISER: %X\n", *ptr);
+        ptr++;
+    }
+
+    return 0;
+}
